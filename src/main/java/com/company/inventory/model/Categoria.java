7eafc21ad41a924ed9c40a9c775516a4a3feee0f
jairoordazmoreno <ordaz_moreno@live.com.mx>
2022-05-09 08:31:16 -0500

    *Cambios de get,post y put para categorias

---
+++ src/main/java/com/company/inventory/model/Categoria.java
@@ -0,0 +1,33 @@
+package com.company.inventory.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+
+}
