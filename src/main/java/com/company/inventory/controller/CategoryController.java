745e8844955be8daf3dcafa8a982a13f39510bdf
jairoordaz <ordaz_moreno@live.com.mx>
2022-05-15 08:14:23 -0500

    crossOrigin

--- src/main/java/com/company/inventory/controller/CategoryController.java
+++ src/main/java/com/company/inventory/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.company.inventory.model.Categoria;
 import com.company.inventory.model.Respuesta;
 import com.company.inventory.service.ICategoriaService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/categorias")
 public class CategoryController {
